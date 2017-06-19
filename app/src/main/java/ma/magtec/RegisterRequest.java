c22fcca35026b0c269fae81fd4b1e4ed41f210d1
bqqbz <magggooo@gmail.com>
2017-06-19 15:58:53 +0200

    - clone of LoginRegister

--- app/src/main/java/ma/magtec/RegisterRequest.java
+++ app/src/main/java/ma/magtec/RegisterRequest.java
@@ -1,13 +1,28 @@
 package ma.magtec;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
 
-public class RegisterRequest extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    //private static final String REGISTER_REQUEST_URL = "http://10.58.28.230/Register.php";
+    private static final String REGISTER_REQUEST_URL = "https://bqqbz.000webhostapp.com/Register.php";
+    //private static final String REGISTER_REQUEST_URL = "http://192.168.10.82/Register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String username, int age, String password, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+        params = new HashMap<>();
+        params.put("name", name);
+        params.put("username", username);
+        params.put("password", password);
+        params.put("age", age + "");
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_request);
+    public Map<String, String> getParams() {
+        return params;
     }
 }
